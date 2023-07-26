e93897bb1564da0d01b98f88f4db8135b449540c
Adanech <aduti.alemu@gmail.com>
2023-07-26 14:48:04 -0700

    Shell utils

---
+++ shell_utils.c
@@ -0,0 +1,38 @@
+/**
+ * active_terminal - checks if shell is active and returns true
+ * @data: pointer to struct
+ * Return: 1: if in active mode, else 0
+ */
+int active_terminal(data_s *data)
+{
+}
+
+/**
+ * _atoi - converts string to integer
+ * @s: the string param
+ * Return: converted numb, O if no number found in string
+ */
+
+int _atoi(char *s)
+{
+}
+
+/**
+ * _isalpha - checks if char is alphabetic
+ * @c: The char param
+ * Return: 1 if @c is alphabetic, else returns 0
+ */
+int _isalpha(int c)
+{
+}
+
+/**
+ * is_delimiter - checks if char is a delimeter.
+ * @c: the char param
+ * @delimeters: string containing chars to compare
+ * Return: 1 if true, 0 if false
+ */
+int is_delimiter(char c, char *delimeters)
+{
+}
+
