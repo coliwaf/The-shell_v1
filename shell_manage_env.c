1a53a11aca24257bc86ae03e6677b760feadf900
Adanech <aduti.alemu@gmail.com>
2023-07-26 15:17:58 -0700

    Managing the shell environment

---
+++ shell_manage_env.c
@@ -0,0 +1,33 @@
+/**
+ * get_environ - returns copy of the environ vars as string array
+ * @data: Struct containing potential args
+ *
+ * Return: array of strings containing environment vars
+ */
+char **get_environ(data_s *data)
+{
+}
+
+/**
+ * _unsetenv - Remove environment var
+ * @data: Struct containing potential args
+ *
+ *  Return: 1 on successful delete, else 0
+ * @var: string env var param
+ */
+int _unsetenv(data_s *data, char *var)
+{
+}
+/**
+ * _setenv - Initialize a new environment variable,
+ *             or modify existing one
+ * @data: Struct containing potential args
+ *
+ * @var: the string env var property
+ * @value: the string env var value
+ *  Return: Always 0
+ */
+int _setenv(data_s *data, char *var, char *value)
+{
+}
+
