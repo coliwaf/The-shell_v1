3a1a052ad1381f075dee28431361eb6c0c57c516
Adanech <aduti.alemu@gmail.com>
2023-07-26 15:20:11 -0700

    Data management

---
+++ shell_manage_data.c
@@ -0,0 +1,26 @@
+/**
+ * clear_data - initializes data_s struct
+ * @data: pointer to struct
+ */
+void clear_data(data_s *data)
+{
+}
+
+/**
+ * set_data - initializes data_s struct
+ * @data: pointer to struct
+ * @argv: arg vector param
+ */
+void set_data(data_s *data, char **argv)
+{
+}
+
+/**
+ * free_data - frees data_s struct fields
+ * @data: pointer to struct
+ * @all: bool param(true) if freeing all fields
+ */
+void free_data(data_s *data, int all)
+{
+}
+
