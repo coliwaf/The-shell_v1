5d2c53941e1f85fef56a28f5cd766633833975fb
Adanech <aduti.alemu@gmail.com>
2023-07-26 14:54:17 -0700

    shell hanle builtins 1

---
+++ shell_handle_buitins1.c
@@ -0,0 +1,50 @@
+/**
+ * unset_alias - sets alias to string
+ * @data: struct param
+ * @str: the string alias
+ * Return: 1 on error, 0 on success
+ */
+int unset_alias(data_s *data, char *str)
+{
+}
+
+/**
+ * set_alias - sets the provided alias to string
+ * @data: struct param
+ * @str: the string alias
+ *
+ * Return: Always 0 on success, 1 on error
+ */
+int set_alias(data_s *data, char *str)
+{
+}
+
+/**
+ * print_alias - prints the provided alias string
+ * @node: the alias param
+ *
+ * Return: Always 0 on success, 1 on error
+ */
+int print_alias(list_s *node)
+{
+}
+
+/**
+ * change_alias - replaces aliases in the tokenized string
+ * @data: the struct param
+ *
+ * Return: 1 if replaced, 0 otherwise
+ */
+int change_alias(data_s *data)
+{
+}
+
+/**
+ * handle_alias - mimics the alias(man alias) builtin
+ * @data: Struct containing potential args
+ *  Return: Always 0
+ */
+int handle_alias(data_s *data)
+{
+}
+
