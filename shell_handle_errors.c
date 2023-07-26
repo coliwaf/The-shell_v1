ded3a6b561f4490442b3514fb2566c8c9af7d695
Adanech <aduti.alemu@gmail.com>
2023-07-26 15:05:19 -0700

    shell handle error functions

---
+++ shell_handle_errors.c
@@ -0,0 +1,44 @@
+/**
+ * err_puts - prints input string
+ * @str: the string to be printed
+ *
+ * Return: Nothing
+ */
+void err_puts(char *str)
+{
+}
+
+/**
+ * err_putchar - writes the char c to stderr
+ * @c: The char to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int err_putchar(char c)
+{
+}
+
+/**
+ * put_file_descr - writes the char c to given file descr
+ * @c: The char to print
+ * @file_descr: The file descriptor to write to
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int put_file_descr(char c, int file_descr)
+{
+}
+
+/**
+ * puts_file_descr - prints input string
+ * @str: the string to be printed
+ * @file_descr: the filedescriptor to write to
+ *
+ * Return: the number of chars put
+ */
+int puts_file_descr(char *str, int file_descr)
+{
+}
+
---
+++ shell_handle_errors1.c
@@ -0,0 +1,54 @@
+/**
+ * numb_atoi_err - converts a string to integer
+ * @s: the string to convert
+ * Return: converted number else 0 if no numbers,
+ *       -1 on error
+ */
+int numb_atoi_err(char *s)
+{
+}
+
+/**
+ * print_error - prints error message
+ * @data: the struct param
+ * @estr: string containing specified error type
+ * Return: 0 if no numbers in string, converted number otherwise
+ *        -1 on error
+ */
+void print_error(data_s *data, char *estr)
+{
+}
+
+/**
+ * print_d - function prints a base 10 integer
+ * @input: the input param
+ * @file_descr: the file descriptor to write to
+ *
+ * Return: number of characters printed
+ */
+int print_d(int input, int file_descr)
+{
+}
+
+/**
+ *  convert_numb_base - converter function, a clone of itoa
+ * @num: number
+ * @base: base
+ * @flags: argument flags
+ *
+ * Return: string
+ */
+char *convert_numb_base(long int num, int base, int flags)
+{
+}
+
+/**
+ * convert_comments - function replaces first instance of '#' with '\0'
+ * @buffr: address of the string to modify
+ *
+ * Return: Always 0;
+ */
+void convert_comments(char *buffr)
+{
+}
+
