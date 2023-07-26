37c610d7a330d784ca09e6b7ed18b7fec255e3ae
Adanech <aduti.alemu@gmail.com>
2023-07-26 15:08:45 -0700

    Shell string functions

---
+++ shell_string_helpers.c
@@ -0,0 +1,32 @@
+/**
+ **_strncpy - copies a string to specified destination
+ *@dest: the destination string param
+ *@src: the source string param
+ *@n: number of characters to be copied
+ *Return: the new full string
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+}
+
+/**
+ **_strncat - concatenates two strings
+ *@dest: the first string
+ *@src: the second string
+ *@n: the amount of bytes to be maximally used
+ *Return: New full string
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+}
+
+/**
+ **_strchr - locates a character in a string
+ *@s: the string to parse
+ *@c: the char parameter to find in @s
+ *Return: pointer to resulting memory area
+ */
+char *_strchr(char *s, char c)
+{
+}
+
