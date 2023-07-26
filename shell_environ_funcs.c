165232fc861e906e7fef7fb9a75633a00df6d36e
Adanech <aduti.alemu@gmail.com>
2023-07-26 14:59:21 -0700

    shell environment functions

---
+++ shell_environ_funcs.c
@@ -0,0 +1,42 @@
+/**
+ * my_env - prints current env
+ * @data: Struct containing potential args
+ *
+ * Return: Always 0
+ */
+int my_env(data_s *data)
+{
+}
+
+/**
+ * env_get - gets val of environ var
+ * @data: Struct containing potential args
+ * @name: env var param
+ *
+ * Return: the value
+ */
+char *env_get(data_s *data, const char *name)
+{
+}
+
+/**
+ * set_my_env - Initialize a new environment variable,
+ * or modify existing one
+ * @data: Struct containing potential args
+ *
+ *  Return: Always 0
+ */
+int set_my_env(data_s *data)
+{
+}
+
+/**
+ * create_env - populates env linked list
+ * @data: Struct containing potential args
+ *
+ * Return: Always 0
+ */
+int create_env(data_s *data)
+{
+}
+
